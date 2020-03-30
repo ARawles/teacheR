a43bda5aa2edb2f7da4f7a911221a1dc039f03fe
ARawles <adamrawles@hotmail.co.uk>
2020-03-30 16:59:27 +0100

    Update Module 1 to xaringan!
    Made multiple formatting changes to accompany the change. The major changes being separating slides with `---` and reimplementing the incremental slides with `--`.

--- main.R
+++ main.R
@@ -1,4 +1,5 @@
 library(rmarkdown)
+library(xaringan)
 
 rmarkdown::render("modules/1_Intro/r_training_intro_presentation.Rmd", output_format = "all")
 rmarkdown::render("modules/2_Data_analysis/r_training_data_analysis_presentation.Rmd", output_format = "all")
