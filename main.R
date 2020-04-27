6783f0679806737f7f97e01bb6d44ce43ebedf3d
ARawles <adamrawles@hotmail.co.uk>
2020-04-27 22:19:26 +0100

    Moved book files to /doc to use GitHub pages

--- main.R
+++ main.R
@@ -1,8 +1,9 @@
 library(rmarkdown)
 library(xaringan)
 library(learnr)
+library(bookdown)
 
-rmarkdown::render("index.Rmd", output_format = "html_document")
+bookdown::render_book("index.Rmd")
 rmarkdown::render("modules/1_Intro/r_training_intro_presentation.Rmd", output_format = "all")
 rmarkdown::render("modules/2_Data_analysis/r_training_data_analysis_presentation.Rmd", output_format = "all")
 rmarkdown::render("modules/3_Programming/r_training_programming_presentation.Rmd", output_format = "all")
