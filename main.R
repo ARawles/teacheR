b52468e701e4780a443620d66ceb09d82b12ae73
ARawles <adamrawles@hotmail.co.uk>
2020-04-27 22:10:28 +0100

    Changed the structure to use bookdown. There is now a link to each presentation in each chapter.

--- main.R
+++ main.R
@@ -2,7 +2,7 @@ library(rmarkdown)
 library(xaringan)
 library(learnr)
 
-rmarkdown::render("index.md", output_format = "html_document")
+rmarkdown::render("index.Rmd", output_format = "html_document")
 rmarkdown::render("modules/1_Intro/r_training_intro_presentation.Rmd", output_format = "all")
 rmarkdown::render("modules/2_Data_analysis/r_training_data_analysis_presentation.Rmd", output_format = "all")
 rmarkdown::render("modules/3_Programming/r_training_programming_presentation.Rmd", output_format = "all")
