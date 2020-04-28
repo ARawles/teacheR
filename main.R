472eb1566216aeb91e53c1dba862fd0a088520ef
ARawles <adamrawles@hotmail.co.uk>
2020-04-28 17:07:02 +0100

    Fixed paths to presentations

--- main.R
+++ main.R
@@ -4,9 +4,9 @@ library(learnr)
 library(bookdown)
 
 bookdown::render_book("index.Rmd")
-rmarkdown::render("modules/1_Intro/r_training_intro_presentation.Rmd", output_format = "all")
-rmarkdown::render("modules/2_Data_analysis/r_training_data_analysis_presentation.Rmd", output_format = "all")
-rmarkdown::render("modules/3_Programming/r_training_programming_presentation.Rmd", output_format = "all")
-rmarkdown::render("modules/4_Tidyverse/r_training_tidyverse.Rmd", output_format = "all")
+rmarkdown::render("modules/2_Intro/r_training_intro_presentation.Rmd", output_format = "all")
+rmarkdown::render("modules/3_Data_analysis/r_training_data_analysis_presentation.Rmd", output_format = "all")
+rmarkdown::render("modules/4_Programming/r_training_programming_presentation.Rmd", output_format = "all")
+rmarkdown::render("modules/6_Tidyverse/r_training_tidyverse.Rmd", output_format = "all")
 rmarkdown::render("exercises/all_modules_exercises/all_modules_exercises.Rmd", output_format = "all")
 
