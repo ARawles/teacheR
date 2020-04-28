7e9f3ab83eeaf63d46873f0ace7f7cdd66d8d4e7
ARawles <adamrawles@hotmail.co.uk>
2020-04-28 10:30:52 +0100

    Added travis CI integration

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # teacheR
 
+  <!-- badges: start -->
+  [![Travis build status](https://travis-ci.org/ARawles/R_Training.svg?branch=master)](https://travis-ci.org/ARawles/R_Training)
+  <!-- badges: end -->
+
 This package holds the training materials for the teacheR book.
+
---
+++ _deploy.sh
@@ -0,0 +1,11 @@
+
+# clone the repository to the book-output directory
+git clone -b gh-pages \
+  https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git \
+  book-output
+cd book-output
+git rm -rf *
+cp -r ../docs/* ./
+git add --all *
+git commit -m"Rebuilt the book"
+git push -q origin gh-pages
