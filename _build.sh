18cc8081f7ca7c22a572dba7e0ca4642a2ce44c8
ARawles <adamrawles@hotmail.co.uk>
2020-04-28 17:06:20 +0100

    Fixed build.sh script which was looking at old folders

--- _build.sh
+++ _build.sh
@@ -2,8 +2,8 @@
 
 bookdown::render_book("index.Rmd", "bookdown::gitbook")
 bookdown::render_book("index.Rmd", "bookdown::pdf_book")
-rmarkdown::render("presentations/1_Intro/r_training_intro_presentation.Rmd", output_format = "all")
-rmarkdown::render("presentations/2_Data_analysis/r_training_data_analysis_presentation.Rmd", output_format = "all")
-rmarkdown::render("presentations/3_Programming/r_training_programming_presentation.Rmd", output_format = "all")
-rmarkdown::render("presentations/4_Tidyverse/r_training_tidyverse.Rmd", output_format = "all")
+rmarkdown::render("presentations/2_Intro/r_training_intro_presentation.Rmd", output_format = "all")
+rmarkdown::render("presentations/3_Data_analysis/r_training_data_analysis_presentation.Rmd", output_format = "all")
+rmarkdown::render("presentations/4_Programming/r_training_programming_presentation.Rmd", output_format = "all")
+rmarkdown::render("presentations/6_Tidyverse/r_training_tidyverse.Rmd", output_format = "all")
 rmarkdown::render("exercises/all_modules_exercises/all_modules_exercises.Rmd", output_format = "all")
