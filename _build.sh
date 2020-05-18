a92689cc1460b9998c69aad3375c09a602246e91
ARawles <adamrawles@hotmail.co.uk>
2020-05-18 13:24:28 +0100

    Added EBook option

--- _build.sh
+++ _build.sh
@@ -2,5 +2,6 @@
 
 bookdown::render_book("index.Rmd", "bookdown::gitbook")
 bookdown::render_book("index.Rmd", "bookdown::pdf_book")
+bookdown::render_book(input = "index.Rmd", output_format = bookdown::epub_book())
 rmarkdown::render("presentations/2_Intro/r_training_intro_presentation.Rmd", output_format = "all")
 rmarkdown::render("exercises/all_modules_exercises/all_modules_exercises.Rmd", output_format = "all")
