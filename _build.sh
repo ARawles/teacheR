d0021ba70062cc16be6b2e75c8c7fcf6e91bc24e
ARawles <adamrawles@hotmail.co.uk>
2020-05-18 14:52:22 +0100

    Added cover to ebook

--- _build.sh
+++ _build.sh
@@ -2,6 +2,6 @@
 
 bookdown::render_book("index.Rmd", "bookdown::gitbook")
 bookdown::render_book("index.Rmd", "bookdown::pdf_book")
-bookdown::render_book(input = "index.Rmd", output_format = bookdown::epub_book())
+bookdown::render_book(input = "index.Rmd", output_format = bookdown::epub_book(cover_image = "cover.png"))
 rmarkdown::render("presentations/2_Intro/r_training_intro_presentation.Rmd", output_format = "all")
 rmarkdown::render("exercises/all_modules_exercises/all_modules_exercises.Rmd", output_format = "all")
