537889aa1e3d28eec31b25f559f1a147be24acd6
ARawles <adamrawles@hotmail.co.uk>
2020-06-16 21:50:32 +0100

    Changed to split by section

--- _build.sh
+++ _build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 
-bookdown::render_book("index.Rmd", "bookdown::gitbook")
+bookdown::render_book("index.Rmd", bookdown::gitbook(split_by = "section"))
 bookdown::render_book("index.Rmd", "bookdown::pdf_book")
 bookdown::render_book(input = "index.Rmd", output_format = bookdown::epub_book(cover_image = "cover.png"))
 rmarkdown::render("presentations/2_Intro/r_training_intro_presentation.Rmd", output_format = "all")
