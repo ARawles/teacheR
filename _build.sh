941089d4cb73965a50f04231e7a6d27e11008400
ARawles <adamrawles@hotmail.co.uk>
2020-06-17 20:57:15 +0100

    Tried to fix CI linebreak issue

--- _build.sh
+++ _build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env Rscript
-
 bookdown::render_book("index.Rmd", bookdown::gitbook(split_by = "section"))
 bookdown::render_book("index.Rmd", "bookdown::pdf_book")
 bookdown::render_book(input = "index.Rmd", output_format = bookdown::epub_book(cover_image = "cover.png"))
