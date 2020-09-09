868b4c9009da2a2b4f610fd83551f87873e260d5
ARawles <adamrawles@hotmail.co.uk>
2020-09-09 14:49:58 +0100

    Update _build.sh
    Adding the contributors obviously needs to be before you render the book but 5-minute-previous me didn't think of that

--- _build.sh
+++ _build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env Rscript
+source("contribs.R")
 bookdown::render_book("index.Rmd", bookdown::gitbook(split_by = "section"))
 bookdown::render_book("index.Rmd", "bookdown::pdf_book")
 bookdown::render_book(input = "index.Rmd", output_format = bookdown::epub_book(cover_image = "cover.png"))
-source("contribs.R")
