1276b39eebbf4519525a1d58e64b7e08b56a58d0
ARawles <adamrawles@hotmail.co.uk>
2020-09-09 14:39:40 +0100

    Update _build.sh
    Added contributor API call

--- _build.sh
+++ _build.sh
@@ -2,4 +2,4 @@
 bookdown::render_book("index.Rmd", bookdown::gitbook(split_by = "section"))
 bookdown::render_book("index.Rmd", "bookdown::pdf_book")
 bookdown::render_book(input = "index.Rmd", output_format = bookdown::epub_book(cover_image = "cover.png"))
-
+source("contribs.R")
